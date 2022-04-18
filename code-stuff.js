d73483f0aba094d4fde9296616279a605f7a319e
SGSouthard <sydneyaileensouthard@gmail.com>
2022-04-18 15:17:53 -0700

    prototype complete

--- code-stuff.js
+++ code-stuff.js
@@ -1,7 +1,16 @@
 let bgText = [
     "An open field",
-    "A dillapidated castle"
+    "A dilapidated castle",
+    "A gloomy cave",
+    "A glimmering city",
 ]
 
 let generateButton = document.getElementById("generate-button");
 
+generateButton.onclick = function() {
+    let textBlock = document.getElementById("text-block");
+    textBlock.textContent = bgText[Math.floor(Math.random() * bgText.length)];
+
+}
+
+console.log("JS is connected and online")
