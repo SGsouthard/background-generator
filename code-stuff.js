5280d69c95a6bd0372752ea2c3eb88e61a3b9e0d
SGSouthard <sydneyaileensouthard@gmail.com>
2022-06-23 18:49:30 -0700

    updated code-stuff.js

--- code-stuff.js
+++ code-stuff.js
@@ -1,24 +1,33 @@
-const bgText = [
-    "An open field",
-    "A dilapidated castle",
-    "A gloomy cave",
-    "A glimmering city",
-]
+// const bgText = [
+//     "An open field",
+//     "A dilapidated castle",
+//     "A gloomy cave",
+//     "A glimmering city",
+// ]
 
 // setting up a set of items to be generated
 const bgGenre = [
     "Fantasy",
     "Modern",
     "SciFi",
+    "Medieval",
+    "Horror",
+    "Mystery",
 ]
-const bgType = []
+const bgLocation = [
+    "Cafe",
+    "School",
+    "Apartment",
+    "House",
+    "Al"
+]
 
 let generateButton = document.getElementById("generate-button");
 
 generateButton.onclick = function() {
-    let textBlock = document.getElementById("text-block");
+    // let textBlock = document.getElementById("text-block");
     let variableLine = document.getElementById("genre-variable");
-    textBlock.textContent = bgText[Math.floor(Math.random() * bgText.length)];
+    // textBlock.textContent = bgText[Math.floor(Math.random() * bgText.length)];
     variableLine.textContent = bgGenre[Math.floor(Math.random() * bgGenre.length)];
 
 }
