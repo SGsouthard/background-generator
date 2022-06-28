70dee0e359291b50230c26e7ec0d2e1125a78d0a
SGSouthard <sydneyaileensouthard@gmail.com>
2022-06-27 22:44:38 -0700

    updated code-stuff.js

--- code-stuff.js
+++ code-stuff.js
@@ -27,14 +27,25 @@ const bgLocation = [
 ]
 
 let generateButton = document.getElementById("generate-button");
+let startAgainButton = document.getElementById("start-again")
 let genreLine = document.getElementById("genre-variable");
 let locationLine = document.getElementById("location-variable");
+let hitOnce = false;
 
 generateButton.onclick = function() {
     genreLine.textContent = bgGenre[Math.floor(Math.random() * bgGenre.length)];
     locationLine.textContent = bgLocation[Math.floor(Math.random() * bgLocation.length)];
     const stopAnimatingGenre = setTimeout(stopAnimateWordsGenre,500);
     const stopAnimatingLocation = setTimeout(stopAnimateWordsLocation,1000);
+    hitOnce = true;
+    if (hitOnce = true) {
+        startAgainButton.classList.remove('hidden');
+        generateButton.classList.add('hidden');
+    }
+}
+
+startAgainButton.onclick = function() {
+    location.reload()
 }
 
 function animateGenre() {
